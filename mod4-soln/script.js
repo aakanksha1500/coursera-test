01de5af7fd99985e8f1fd7127dea9e5a472e7397
Aakanksha <aakankshadeshpande527@gmail.com>
2020-07-22 01:16:20 +0530

    error solved

--- mod4-soln/script.js
+++ mod4-soln/script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-//(function () {
+
 
 //var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -40,7 +40,7 @@ WARNING!!! WARNING!!!
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-//for (var i = 0; i < names.length; i++) {
+
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -56,14 +56,7 @@ WARNING!!! WARNING!!!
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
- // if (firstLetter === 'j') {
-    //byeSpeaker.speak(names[i]);
- // } else {
-   // helloSpeaker.speak(names[i]);
- // }
-//}
 
-//})();
 (function() {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
